ede57e888f1621629c80e2e62bcb38361a5b5b64
Andrei Larionov <0658203@gmail.com>
2023-05-11 15:17:02 -0300

    KS-1-2 adds the number of workers and displays the date set

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,14 @@
+package lesson_1
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val notificationText: String = "Спасибо за покупку"
+    var employeeCount: Int = 2000
+
+    println(numberOfOrders)
+    println(notificationText)
+//    println(employeeCount )
+    employeeCount--
+    println(employeeCount)
+}
