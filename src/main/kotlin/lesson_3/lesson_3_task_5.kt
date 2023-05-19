efd60cab438951270ef48ba2095028ccc87d256c
Andrei Larionov <0658203@gmail.com>
2023-05-19 12:14:17 -0300

    KS-3-5 rewrites the solution in Kotlin from lesson 5

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -1,9 +1,4 @@
 package lesson_3
-
-import lesson_3.MoveCount.Companion.moveCount
-import java.util.regex.Matcher
-import java.util.regex.Pattern
-
 /*Продолжаем работать над приложением “Шахматы”. Напиши программу для сервера, которая примет строку,
 отправленную игроком (считаем эти данные уже известными и объявляем строку с данными: “D2-D4;0”.
 
@@ -14,45 +9,29 @@ fun main() {
 
     var valueFrom: String = "E2"
     var valueTo: String = "E4"
+    var moveCount: Int = 0
 
-    println(getChessMove(valueFrom, valueTo))
+    println(getChessMove(valueFrom, valueTo, ++moveCount))
 
-    // игрок делает ход
     valueFrom = "D2"
     valueTo = "D4"
 
-    // сервер получил ход игрока
-    val movePlayer = getChessMove(valueFrom, valueTo)
-    unparseChessMove(movePlayer)
+    parseServer(getChessMove(valueFrom, valueTo, ++moveCount))
+
 }
 
-// используем регулярное выражение, чтобы перехватить 10 ход и тд
-fun unparseChessMove(move: String) {
-    val regex = """([A-H][1-8])-([A-H][1-8]);(\d+)"""
-    val pattern: Pattern = Pattern.compile(regex)
-    val matcher: Matcher = pattern.matcher(move)
+fun getChessMove(from: String, to: String, moveCount: Int) = "[$from-$to;$moveCount]"
 
-    if (matcher.find()) {
-        val valueFromServer = matcher.group(1)
-        val valueToServer = matcher.group(2)
-        val moveCountServer = matcher.group(3)
+fun parseServer(move: String) {
 
-        println(valueFromServer)
-        println(valueToServer)
-        println(moveCountServer)
-    } else {
-        println("так ходить нельзя")
+    val regex = Regex("([A-Z].+)-([A-Z].+);(.*)]")
+    val matchResult = regex.find(move)
+    if (matchResult != null) {
+        val (valueFrom, valueTo, moveCount) = matchResult.destructured
+        println("""
+            $valueFrom
+            $valueTo
+            $moveCount
+        """.trimIndent())
     }
 }
-
-fun getChessMove(from: String, to: String): String {
-    moveCount++
-    return "[$from-$to;$moveCount]"
-}
-
-class MoveCount {
-    companion object {
-        var moveCount: Int = 0
-    }
-}
-
