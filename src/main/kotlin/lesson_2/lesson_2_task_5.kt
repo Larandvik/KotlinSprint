5eb8af38669dbf587e83e62382dc9b00f24fe1ab
Andrei Larionov <0658203@gmail.com>
2023-05-19 12:24:40 -0300

    KS-2-5 removes unnecessary imports from lesson 2

--- src/main/kotlin/lesson_2/lesson_2_task_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -1,8 +1,5 @@
 package lesson_2
-
 import kotlin.math.pow
-import kotlin.Unit as Unit1
-
 /*Напиши программу-калькулятор для банковского приложения (такой функционал бывает на промо экранах).
 Используя формулу сложных процентов (ее можно загуглить),
 нужно посчитать размер вклада через 20 лет с такими условиями:
@@ -12,6 +9,7 @@ import kotlin.Unit as Unit1
  – Подсчитать размер вклада через 20 лет;
  – Результат должен быть дробным числом с 3 знаками после запятой;
  – Ответ должен получиться таким 1536438.428.*/
+
 fun main() {
 
     val initialAmount: Double = 70000.0
@@ -19,6 +17,5 @@ fun main() {
     val years: Int = 20
 
     val finalAmount = String.format ("%.3f", initialAmount * (1 + interestRate / 100).pow(years.toDouble()))
-//    println(finalAmount)
-
+    println(finalAmount)
 }
