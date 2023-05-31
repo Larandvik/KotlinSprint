12c907372feaaf3b33191bc434f1f84451541c39
Andrei Larionov <0658203@gmail.com>
2023-05-31 13:02:35 -0300

    KS-12-4 cleans code

--- src/main/kotlin/lesson_12/lesson_12_task_4.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_4.kt
@@ -1,14 +1,12 @@
 package lesson_12
+
 /*Измени программу таким образом, чтобы информация о погоде выводилась в консоль сразу при создании объекта.*/
 fun main() {
-    val weatherToday = WeatherTracker3(21,9,false,733)
-    val weatherTomorrow = WeatherTracker3(25,10,true,738)
-
-//    weatherToday.printWeather2()
-//    weatherTomorrow.printWeather2()
+    val weatherToday = WeatherTracker3(21, 9, false, 733)
+    val weatherTomorrow = WeatherTracker3(25, 10, true, 738)
 }
 
-class WeatherTracker3 (
+class WeatherTracker3(
     val temperatureDay: Int,
     val temperatureNight: Int,
     val isRainy: Boolean = false,
@@ -19,12 +17,14 @@ class WeatherTracker3 (
     }
 
     fun printWeather2() {
-        println("""
+        println(
+            """
             погода днём: $temperatureDay
             погода ночью: $temperatureNight
             вероятность дождя: $isRainy
             атмосферное давление: $atmosphericPressure
             
-        """.trimIndent())
+        """.trimIndent()
+        )
     }
 }
