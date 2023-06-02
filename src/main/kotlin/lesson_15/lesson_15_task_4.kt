6ce048c54c42c94171488c9f1d4c98f85693660a
Andrei Larionov <0658203@gmail.com>
2023-06-02 19:31:10 -0300

    KS-15-4 solves task of music market from lesson 15

--- src/main/kotlin/lesson_15/lesson_15_task_4.kt
+++ src/main/kotlin/lesson_15/lesson_15_task_4.kt
@@ -4,3 +4,35 @@ package lesson_15
 У всех товаров есть название и количество единиц на складе.
 Напиши классы для категорий товаров. В качестве поиска – отобрази сообщение «Выполняется поиск».
 Метод поиска реализуй через интерфейс.*/
+fun main() {
+    val guitar = MusicInstrument("Fender", 5)
+    val strings = MusicPart("Струны", 100)
+    guitar.searchPart()
+}
+
+abstract class Goods(
+    val name: String,
+    val quantity: Int,
+)
+
+class MusicInstrument(name: String, quantity: Int) : Goods(name, quantity), SearchAble {
+
+    init {
+        println("добавили в базу гитары")
+    }
+
+    override fun searchPart() {
+        println("Выполняется поиск")
+    }
+}
+
+class MusicPart(name: String, quantity: Int) : Goods(name, quantity) {
+
+    init {
+        println("добавили в базу струны")
+    }
+}
+
+interface SearchAble {
+    fun searchPart()
+}
