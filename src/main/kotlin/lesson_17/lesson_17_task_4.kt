25626d2810b196a468bf2de2ccf2dce055d8c355
Andrei Larionov <0658203@gmail.com>
2023-06-06 22:14:22 -0300

    KS-17-4 solves task of Package from lesson 17

---
+++ src/main/kotlin/lesson_17/lesson_17_task_4.kt
@@ -0,0 +1,33 @@
+package lesson_17
+
+/*Каждый раз, когда посылка прибывает в промежуточный пункт обработки, в программе меняется её местоположение.
+Нужно написать класс посылки. Должно быть неизменяемое поле с номером и поле с текущим местоположением в виде строки.
+Также должен быть счетчик перемещений, обновляющийся при перезаписи местоположения.
+Сымитировать прибытие посылки в новый пункт обработки, протестировать изменение счетчика.
+При реализации использовать сеттер.*/
+fun main() {
+    val package1 = Package(1234, "Москва")
+
+    package1.currentLocation = "Владимир"
+    println(package1.currentLocation)
+    package1.currentLocation = "Воронеж"
+    println(package1.currentLocation)
+}
+
+class Package(
+
+    private val parcelNumber: Int,
+    private var location: String,
+) {
+
+    private var counterLocation: Int = 1
+
+    var currentLocation: String
+        get() = location
+        set(value) {
+            location = value
+            counterLocation++
+            println(counterLocation)
+        }
+}
+
