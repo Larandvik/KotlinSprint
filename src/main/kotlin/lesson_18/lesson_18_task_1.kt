48eb5f476548ed5ff2420c109546efefab3a476a
Andrei Larionov <0658203@gmail.com>
2023-06-08 16:57:10 -0300

    KS-18-1 solves task of Orders from lesson 18

---
+++ src/main/kotlin/lesson_18/lesson_18_task_1.kt
@@ -0,0 +1,47 @@
+package lesson_18
+
+/*CRM (система управления взаимоотношениями с клиентами) обрабатывает заказы в интернет-магазине.
+Заказ содержит информацию: номер заказа и товары.
+Если товар один, он хранится в виде строки, если несколько – в виде списка строк.
+
+По запросу нужно печатать в консоль сообщение
+«Заказан товар: [название_товара]» или
+«Заказаны следующие товары: [название_товара], … [название_товара]».
+Создай 2 объекта заказа и вызови у них метод вывода информации и заказе. Реши задачу, используя полиморфизм.*/
+fun main() {
+    val singleOrder: Order181 = SingleOrder181(1, "носки")
+    singleOrder.printOrderInfo()
+
+    val multipleOrder: Order181 = MultipleOrder181(2, listOf("носки", "майка", "футболка"))
+    multipleOrder.printOrderInfo()
+}
+
+open class Order181 {
+    open fun printOrderInfo() {
+        println()
+    }
+}
+
+class SingleOrder181(
+    numberOrder: Int,
+    item: String,
+) : Order181() {
+
+    private val item = item
+
+    override fun printOrderInfo() {
+        println("Заказан товар: $item")
+    }
+}
+
+class MultipleOrder181(
+    numberOrder: Int,
+    items: List<String>,
+) : Order181() {
+
+    private val items = items
+
+    override fun printOrderInfo() {
+        println("Заказаны следующие товары: $items")
+    }
+}
