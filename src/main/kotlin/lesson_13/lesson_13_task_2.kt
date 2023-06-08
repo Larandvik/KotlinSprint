49d8128119c6a896a65486bc17c315174f1b3d5f
Andrei Larionov <0658203@gmail.com>
2023-06-07 22:30:58 -0300

    KS-13-2 cleans code

--- src/main/kotlin/lesson_13/lesson_13_task_2.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_2.kt
@@ -1,37 +1,30 @@
 package lesson_13
 /*Используй класс из первой задачи.
 На его основе создай экземпляр. Вместо поля с названием компании пропиши null при инициализации объекта.
-
 Дополни класс методом, который будет печатать информацию о контакте сообщением такого типа (пример):
-
 Имя: Ростислав
 Номер: 89123456789
 Компания: Reddit
-
 В одном println() и без использования многострочного ввода.
 Вместо null значения, в консоль должна выводиться строка [не указано].*/
 
 fun main() {
-    val contact1 = Contact2("Nikita", 8_925_000_00_09, null)
+    val contact1 = Contact2("Nikita", 8_925_000_00_09)
     val contact2 = Contact2("Ростислав", 89123456789, "Reddit")
     println(contact1)
     println(contact2)
 }
 
-class Contact2(_name: String, _phone: Long, _company: String?) {
+class Contact2(
+    private val name: String,
+    private val phone: Long,
+    company: String? = null,
+) {
 
-    private val name = _name
-    private val phone = _phone
-    private val company = _company ?: "не указано"
-
+    private val company = company ?: "не указано"
 
     override fun toString(): String {
-        return """
-            Имя: $name
-            Номер: $phone
-            Компания: $company
-            
-        """.trimIndent()
+        return "Имя: $name\nНомер: $phone\nКомпания: $company\n"
     }
 }
 
