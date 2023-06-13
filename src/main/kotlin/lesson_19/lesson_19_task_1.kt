fcb6b8c5bd015cefbf459d1865d91894a2294552
Andrei Larionov <0658203@gmail.com>
2023-06-12 21:44:34 -0300

    KS-19-1 cleans code

--- src/main/kotlin/lesson_19/lesson_19_task_1.kt
+++ src/main/kotlin/lesson_19/lesson_19_task_1.kt
@@ -4,30 +4,14 @@ package lesson_19
 Напиши enum класс, перечисляющий этих рыб.
 Сообщи пользователю, каких рыб он может добавить в свой аквариум, распечатав список в консоль.*/
 fun main() {
-    for (i in Fish.values()) println(i.getFishName())
+    println(Fish.values())
+    for (i in Fish.values()) println(i.fishName)
 }
 
-enum class Fish {
-    GUPPY {
-        override fun getFishName(): String {
-            return "guppy"
-        }
-    },
-    ANGELFISH {
-        override fun getFishName(): String {
-            return "angelfish"
-        }
-    },
-    GOLDFISH {
-        override fun getFishName(): String {
-            return "Goldfish"
-        }
-    },
-    SIAMESE_FIGHTING_FISH {
-        override fun getFishName(): String {
-            return "Siamese fighting fish"
-        }
-    };
+enum class Fish(val fishName: String) {
 
-    abstract fun getFishName() : String
+    GUPPY("guppy"),
+    ANGELFISH("angelfish"),
+    GOLDFISH("Goldfish"),
+    SIAMESE_FIGHTING_FISH("Siamese fighting fish"),
 }
